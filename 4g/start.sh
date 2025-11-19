98387075c721af0727f3ed97a60e1756793c2471
nwiuser <nouisserzakariae@gmail.com>
2025-11-19 12:03:29 +0100

    update start.sh

--- 4g/start.sh
+++ 4g/start.sh
@@ -3,5 +3,21 @@
 # Build the Docker images
 docker-compose build
 
+# Check if containers already exist and remove them if they do
+if [ "$(docker ps -aq -f name=virtual-srsepc)" ]; then
+    echo "Removing existing virtual-srsepc container..."
+    docker rm -f virtual-srsepc
+fi
+
+if [ "$(docker ps -aq -f name=virtual-srsenb)" ]; then
+    echo "Removing existing virtual-srsenb container..."
+    docker rm -f virtual-srsenb
+fi
+
+if [ "$(docker ps -aq -f name=virtual-srsue)" ]; then
+    echo "Removing existing virtual-srsue container..."
+    docker rm -f virtual-srsue
+fi
+
 # Start the Docker containers in the background
 docker-compose up
